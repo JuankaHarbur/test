6cf5c9fa50c69d7d6f8b7d2ff41fbac203626c80
juankaNH <juanka@harbur.io>
2020-10-28 19:52:18 +0100

    .

--- app-movies/run.sh
+++ app-movies/run.sh
@@ -2,5 +2,9 @@
 
 echo "EXICUTING JMETER"
 echo ""
+timestamp="$(date +"%Y-%m-%d_%H-%M-%S")"
+pathresult=result_$timestamp
 
-jmeter -p test/app-movies/default.properties -n -t test/app-movies/test.jmx
+jmeter -p default.properties -J pathresult=$pathresult -n -t test.jmx -j $pathresult/jmeter.log
+
+jmeter -g $pathresult/reporthtml.csv -o $pathresult/html
