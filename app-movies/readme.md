f404b487ddbf40c16d961baadcf95d3b0af8b722
juankaNH <juanka@harbur.io>
2020-10-28 22:45:21 +0100

    .

--- app-movies/readme.md
+++ app-movies/readme.md
@@ -1,2 +1,3 @@
 kubectl create configmap <CONFIGMAP_NAME>  --from-file=<FOLDER_WITH_THE_FILES>
 kubectl create configmap jmeter-run-movies --from-file=prerun.sh -n harbur -o yaml
+
--- app-movies/run.sh
+++ app-movies/run.sh
@@ -3,42 +3,42 @@
 echo "EXICUTING JMETER"
 echo ""
 timestamp="$(date +"%Y-%m-%d_%H-%M-%S")"
-pathresult=/result/result_$timestamp
+pathresult=/usr/share/nginx/html/result_$timestamp
 
 jmeter -p test/app-movies/default.properties -J pathresult=$pathresult -n -t test/app-movies/test.jmx -j $pathresult/jmeter.log
 
 jmeter -g $pathresult/reporthtml.csv -o $pathresult/html
-echo "<tr><td><a href='$pathresult/html/index.html'>$pathresult</a></td></tr>" >> /result/pathindex.txt
+echo "<tr><td><a href='$pathresult/html/index.html'>$pathresult</a></td></tr>" >> /usr/share/nginx/html/pathindex.txt
 
-if [ -f /result/index.html ];
+if [ -f /usr/share/nginx/html/index.html ];
 then
-rm -r /result/index.html
+rm -r /usr/share/nginx/html/index.html
 fi
-echo "<!doctype html>" > /result/index.html
-echo "<html lang='es'>" >> /result/index.html
-echo "<head>" >> /result/index.html
-echo "<title>Result test Page</title>" >> /result/index.html
-echo "</head>" >> /result/index.html
-echo "<body>" >> /result/index.html
-echo "<div class='container'>" >> /result/index.html
-echo "<div class='row'>" >> /result/index.html
-echo "<div class='col 4'>" >> /result/index.html
-echo "<h1>Resultados:</h1>" >> /result/index.html
-echo "<table class='table'>" >> /result/index.html
-echo "<thead>" >> /result/index.html
-echo "<tr>" >> /result/index.html
-echo "<th scope='col'>Name</th>" >> /result/index.html
-echo "</tr>" >> /result/index.html
-echo "</thead>" >> /result/index.html
-echo "<tbody>" >> /result/index.html
-if [ -f /result/pathindex.txt ];
+echo "<!doctype html>" > /usr/share/nginx/html/index.html
+echo "<html lang='es'>" >> /usr/share/nginx/html/index.html
+echo "<head>" >> /usr/share/nginx/html/index.html
+echo "<title>Result test Page</title>" >> /usr/share/nginx/html/index.html
+echo "</head>" >> /usr/share/nginx/html/index.html
+echo "<body>" >> /usr/share/nginx/html/index.html
+echo "<div class='container'>" >> /usr/share/nginx/html/index.html
+echo "<div class='row'>" >> /usr/share/nginx/html/index.html
+echo "<div class='col 4'>" >> /usr/share/nginx/html/index.html
+echo "<h1>Resultados:</h1>" >> /usr/share/nginx/html/index.html
+echo "<table class='table'>" >> /usr/share/nginx/html/index.html
+echo "<thead>" >> /usr/share/nginx/html/index.html
+echo "<tr>" >> /usr/share/nginx/html/index.html
+echo "<th scope='col'>Name</th>" >> /usr/share/nginx/html/index.html
+echo "</tr>" >> /usr/share/nginx/html/index.html
+echo "</thead>" >> /usr/share/nginx/html/index.html
+echo "<tbody>" >> /usr/share/nginx/html/index.html
+if [ -f /usr/share/nginx/html/pathindex.txt ];
 then
 while read linea; do
-	echo -e "$linea" >> /result/index.html
-done < /result/pathindex.txt
+	echo -e "$linea" >> /usr/share/nginx/html/index.html
+done < /usr/share/nginx/html/pathindex.txt
 fi
-echo "</tbody>" >> /result/index.html
-echo "</div>" >> /result/index.html
-echo "</div>" >> /result/index.html
-echo "</div>" >> /result/index.html
-echo "</html>" >> /result/index.html
+echo "</tbody>" >> /usr/share/nginx/html/index.html
+echo "</div>" >> /usr/share/nginx/html/index.html
+echo "</div>" >> /usr/share/nginx/html/index.html
+echo "</div>" >> /usr/share/nginx/html/index.html
+echo "</html>" >> /usr/share/nginx/html/index.html
